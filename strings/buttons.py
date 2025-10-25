b9d6319c6295eb5bdba35987c7a0357e5a3a24cb
⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>
2025-10-25 06:06:24 +0530

    Update buttons.py

--- strings/buttons.py
+++ strings/buttons.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    ABUTTON = [[InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/oldskoolgc")],[InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/ixasta1"),
-    InlineKeyboardButton("˹ ᴧʟʟ ʙᴏᴛ ˼", url="https://t.me/https://t.me/ixasta1/69")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
+    ABUTTON = [[InlineKeyboardButton("⌯ sᴜᴘᴘᴏꝛᴛ ⌯", url="https://t.me/oldskoolgc")],[InlineKeyboardButton("⌯ υᴘᴅᴧᴛᴇ ⌯", url="https://t.me/ixasta1"),
+    InlineKeyboardButton("⌯ ᴧʟʟ ʙᴏᴛ ⌯", url="https://t.me/https://t.me/ixasta1/69")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
     ]]
 
-    UBUTTON = [[InlineKeyboardButton("˹ ᴍᴜsɪᴄ ˼", callback_data="settings_back_helper"),InlineKeyboardButton("˹ ᴛᴏᴏʟs ˼", callback_data=f"mbot_cb")],[InlineKeyboardButton("˹ ᴍᴧɴᴧɢᴇ ˼", callback_data=f"bbot_cb"),
-    InlineKeyboardButton("˹ ʀᴧɪᴅ ˼", callback_data="cplus HELP_raid")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
+    UBUTTON = [[InlineKeyboardButton("⌯ ϻυѕɪᴄ ⌯", callback_data="settings_back_helper"),InlineKeyboardButton("⌯ ᴛσσʟs ⌯", callback_data=f"mbot_cb")],[InlineKeyboardButton("˹ ϻᴧɴᴧɢᴇ ˼", callback_data=f"bbot_cb"),
+    InlineKeyboardButton("⌯ ʀᴧɪᴅ ⌯", callback_data="cplus HELP_raid")],[InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"settingsback_helper"),
     ]]
