01eeb543c861410b8c1e18779ddbfae98dd9e693
⋏ 𝛅 𝛕 ⋏ <astakhan63@gmail.com>
2026-08-12 05:30:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,18 +20,13 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002029289902))
 OWNER_ID = int(getenv("OWNER_ID", 5909658683))
 
-HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
-HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+
 API_URL = getenv("API_URL", "https://pvtz.nexgenbots.xyz")
-
-
 VIDEO_API_URL = getenv("VIDEO_API_URL", "https://api.video.nexgenbots.xyz")
 
-
 API_KEY = getenv("API_KEY", "NxGBNexGenBots1b1233")
-
-
-
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
@@ -52,7 +47,9 @@ AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "9000"))
 
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", "9999999"))
-SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(
+    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999")
+)
 
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET")
@@ -60,8 +57,12 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET")
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", 25))
 
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
+TG_AUDIO_FILESIZE_LIMIT = int(
+    getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000")
+)
+TG_VIDEO_FILESIZE_LIMIT = int(
+    getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000")
+)
 
 STRING1 = getenv("STRING_SESSION")
 STRING2 = getenv("STRING_SESSION2")
@@ -78,8 +79,14 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
-START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/fu6jk3.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/26nzoq.jpg")
+START_IMG_URL = getenv(
+    "START_IMG_URL",
+    "https://files.catbox.moe/fu6jk3.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL",
+    "https://files.catbox.moe/26nzoq.jpg"
+)
 PLAYLIST_IMG_URL = "https://files.catbox.moe/f2s4ws.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/z0gh23.jpg"
 
@@ -105,12 +112,16 @@ def time_to_seconds(time):
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
-if SUPPORT_CHANNEL and not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+if SUPPORT_CHANNEL and not re.match(
+    "(?:http|https)://", SUPPORT_CHANNEL
+):
     raise SystemExit(
         "[ERROR] - SUPPORT_CHANNEL must start with https://"
     )
 
-if SUPPORT_CHAT and not re.match("(?:http|https)://", SUPPORT_CHAT):
+if SUPPORT_CHAT and not re.match(
+    "(?:http|https)://", SUPPORT_CHAT
+):
     raise SystemExit(
         "[ERROR] - SUPPORT_CHAT must start with https://"
     )
